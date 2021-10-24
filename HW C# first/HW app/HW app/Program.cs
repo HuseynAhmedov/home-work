8cba4e43fe726801dc85a482312d5c5e35686207
HuseynAhmedov <gold.bullet.767@gmail.com>
2021-10-24 23:57:14 +0400

    jump and fibo pptx file added // HW C# first file added

---
+++ HW C# first/HW app/HW app/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace HW_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //<-------------------------------------------------------------------------------->
+            //[ 1 ] Verilmis array-deki reqemlerinin cemi 10-dan kicik olan ededlerin sayini tapan alqoritm
+
+            //int[] num_arr = new int[10] { 45,22,31,67,12,42,98,56,43,20 };
+            //int num_total = 0;
+            //int num_count = 0;
+            //int digit1 = 0;
+            //int temp = 0;
+            //for (int i = 0; i < num_arr.Length; i++)
+            //{
+            //    temp = num_arr[i];
+            //    while (temp > 0)
+            //    {
+            //        digit1 = temp % 10;
+            //        num_total += digit1;
+            //        temp -= digit1;
+            //        temp /= 10;
+            //    }
+            //    if (num_total < 10)
+            //    {
+            //        num_count++;
+            //    }
+            //    num_total = 0;
+            //}
+            //Console.WriteLine("ededlerinin cemi 10 dan kicik olanlarin sayi : " + num_count);
+
+            //<-------------------------------------------------------------------------------->
+            //[ 2 ] 1-den 8000-dek ededler icerisinde cut olanlarin sayini tapan proqram
+
+            //int even_count = 0;
+            //for (int i = 1; i < 8000; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        even_count++;
+            //    }
+            //}
+            //Console.WriteLine("1 den 8000 e geder icerisinde cut olanlarin sayi : " + even_count);
+
+            //<-------------------------------------------------------------------------------->
+            //[ 3 ] Verilmis ededin en yaxin asagi kokalti deyerini tapan alqoritm
+
+            //int given = 25;
+            //int i = 1;
+            //while (given >= i*i)
+            //{
+            //    i++;
+            //}
+            //Console.WriteLine("Verilmis edede ( " + given + " ) en yaxin asagi kokalti : " + --i);
+
+            //<-------------------------------------------------------------------------------->
+        }
+    }
+}
