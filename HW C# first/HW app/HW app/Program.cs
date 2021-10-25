c1de70f0820b8aef49c24e690db55ea5149ec31c
HuseynAhmedov <gold.bullet.767@gmail.com>
2021-10-26 02:26:04 +0400

    deleted jump and fibo pptx // added HW C# practice

--- HW C# first/HW app/HW app/Program.cs
+++ HW C# first/HW app/HW app/Program.cs
@@ -7,56 +7,50 @@ namespace HW_app
         static void Main(string[] args)
         {
             //<-------------------------------------------------------------------------------->
-            //[ 1 ] Verilmis array-deki reqemlerinin cemi 10-dan kicik olan ededlerin sayini tapan alqoritm
+            //[ 1 ] Daxil edilmis string deyerinin icinde reqem olub olmadigini tapan proqram
 
-            //int[] num_arr = new int[10] { 45,22,31,67,12,42,98,56,43,20 };
-            //int num_total = 0;
-            //int num_count = 0;
-            //int digit1 = 0;
-            //int temp = 0;
-            //for (int i = 0; i < num_arr.Length; i++)
+            //string newstr = "April 15, 1989";
+            //char[] char_num = new char[10] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            //bool contains_num = false;
+
+            //for (int i = 0; i < newstr.Length; i++)
             //{
-            //    temp = num_arr[i];
-            //    while (temp > 0)
+            //    for (int j = 0; j < char_num.Length; j++)
             //    {
-            //        digit1 = temp % 10;
-            //        num_total += digit1;
-            //        temp -= digit1;
-            //        temp /= 10;
-            //    }
-            //    if (num_total < 10)
-            //    {
-            //        num_count++;
-            //    }
-            //    num_total = 0;
-            //}
-            //Console.WriteLine("ededlerinin cemi 10 dan kicik olanlarin sayi : " + num_count);
-
-            //<-------------------------------------------------------------------------------->
-            //[ 2 ] 1-den 8000-dek ededler icerisinde cut olanlarin sayini tapan proqram
-
-            //int even_count = 0;
-            //for (int i = 1; i < 8000; i++)
-            //{
-            //    if (i % 2 == 0)
-            //    {
-            //        even_count++;
+            //        if (newstr[i] == char_num[j])
+            //        {
+            //            contains_num = true;
+            //            break;
+            //        }
             //    }
             //}
-            //Console.WriteLine("1 den 8000 e geder icerisinde cut olanlarin sayi : " + even_count);
+            //if (contains_num)
+            //    Console.WriteLine("Terkibinde regem var");
+            //else
+            //    Console.WriteLine("Terkibinde regem yoxdur");
 
             //<-------------------------------------------------------------------------------->
-            //[ 3 ] Verilmis ededin en yaxin asagi kokalti deyerini tapan alqoritm
+            //[ 2 ] Daxil edilmis string deyerinde ardicil a ve l charlarinin olub olmadigini tapan proqram.Misalcun alma, salam ve s. kimi sozlerde "vardir" yazdirmalidir geriye.
 
-            //int given = 25;
-            //int i = 1;
-            //while (given >= i*i)
+            //string newstr = "burda salam demelisen ";
+            //bool contains_al = false;
+            //for (int i = 0; i < newstr.Length; i++)
             //{
-            //    i++;
+            //    if (newstr[i] == 'a' || newstr[i] == 'A')
+            //    {
+            //        if (newstr[i+1] == 'l' || newstr[i+1] == 'L')
+            //        {
+            //            contains_al = true;
+            //        }
+            //    }
             //}
-            //Console.WriteLine("Verilmis edede ( " + given + " ) en yaxin asagi kokalti : " + --i);
+            //if (contains_al)
+            //    Console.WriteLine("Terkibinde al var");
+            //else
+            //    Console.WriteLine("Terkibinde al yoxdur");
 
             //<-------------------------------------------------------------------------------->
+
         }
     }
 }
---
+++ HW C# practice/HW app/HW app/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace HW_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //<-------------------------------------------------------------------------------->
+            //[ 1 ] Daxil edilmis string deyerinin icinde reqem olub olmadigini tapan proqram
+
+            //string newstr = "April 15, 1989";
+            //char[] arr_char09 = new char[10] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            //bool contains_num = false;
+
+            //for (int i = 0; i < newstr.Length; i++)
+            //{
+            //    for (int j = 0; j < arr_char09.Length; j++)
+            //    {
+            //        if (newstr[i] == arr_char09[j])
+            //        {
+            //            contains_num = true;
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //if (contains_num)
+            //    Console.WriteLine("Terkibinde regem var ");
+            //else
+            //    Console.WriteLine("Terkibinde regem yoxdur ");
+
+            //<-------------------------------------------------------------------------------->
+            //[ 2 ]Daxil edilmis string deyerinde ardicil a ve l charlarinin olub olmadigini tapan proqram.Misalcun alma, salam ve s. kimi sozlerde "vardir" yazdirmalidir geriye.
+
+            //string newstr = " Allies retreat from dunkirk ";
+            //bool contains_al = false;
+            //for (int i = 0; i < newstr.Length; i++)
+            //{
+            //    if (newstr[i] == 'a' || newstr[i] == 'A' )
+            //    {
+            //        if (newstr[i+1] == 'l' || newstr[i+1] == 'L')
+            //        {
+            //            contains_al = true;
+            //            break;
+            //        }
+            //    }
+            //}
+            //if (contains_al)
+            //    Console.WriteLine("Terkibinde al var ");
+            //else
+            //    Console.WriteLine("Terkibinde al yoxdur ");
+
+            //<-------------------------------------------------------------------------------->
+            //[ 3 ] Verilmis ededin reqemlerinin ededi ortasini tapan proqram
+
+            //int num = 126;
+            //int digit1 = 0;
+            //int counter = 0;
+            //int num_total = 0;
+            //while (num > 0)
+            //{
+            //    digit1 = num % 10;
+            //    counter++;
+            //    num_total += digit1;
+            //    num = (num - digit1) / 10;
+            //}
+            //int ededi_orta = num_total / counter;
+            //Console.WriteLine("Verilen ededin ededi ortasi : " + ededi_orta);
+
+            //<-------------------------------------------------------------------------------->
+        }
+    }
+}
