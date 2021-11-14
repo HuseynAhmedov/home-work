690c11c56ad88be4cee7a6767dac986a85130217
HuseynAhmedov <gold.bullet.767@gmail.com>
2021-11-14 19:05:46 +0400

    HW json file added

---
+++ HW json file/HW app/HW app/Program.cs
@@ -0,0 +1,92 @@
+﻿
+using Nancy.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HW_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------------------------------------------
+            //[ 1 ]
+
+            //List<Student> students = new List<Student>();
+            //students.Add(new Student("Huseyn", "Ahmedov", 10));
+            //students.Add(new Student("Huseyn1", "Ahmedov1", 11));
+            //students.Add(new Student("Huseyn2", "Ahmedov2", 12));
+            //students.Add(new Student("Huseyn3", "Ahmedov3", 13));
+
+            //string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //if (!File.Exists(path + @"\Json.txt"))
+            //{
+            //    FileStream stream = new FileStream(path + @"\Json.txt", FileMode.CreateNew);
+            //    stream.Close();
+            //}
+            //DirectoryInfo jsonDirInfo = new DirectoryInfo(path + @"\Json.txt");
+            //SerializeJson(students , jsonDirInfo);
+            //DeSerializeJson(jsonDirInfo);
+
+            //------------------------------------------------------------
+            //[ 2 ]
+
+            //string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //Console.Write(" Enter file name : ");
+            //path += @"\" + Console.ReadLine();
+
+            //while (!Directory.Exists(path))
+            //{
+            //    path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //    Console.Write("\n Given path does not exsist \n Enter file name : ");
+            //    path += @"\" + Console.ReadLine().Trim();
+            //}
+            //GetDirectories(path);
+
+            //------------------------------------------------------------
+        }
+
+        public static void SerializeJson (object toSerialize , DirectoryInfo jsonDirInfo)
+        {
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            string txt = "";
+            
+            txt = json.Serialize(toSerialize);
+            File.WriteAllText(jsonDirInfo.ToString(), txt);
+        }
+
+        public static List<Student> DeSerializeJson (DirectoryInfo jsonDirInfo)
+        {
+            List<Student> students = new List<Student>();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            using (StreamReader sr = new StreamReader(jsonDirInfo.ToString()))
+            {
+                students = js.Deserialize<List<Student>>(sr.ReadToEnd());
+            }
+            return students;
+        }
+
+        public static void GetDirectories(string path)
+        {
+            List<string> dirList =  new List<string>(Directory.GetDirectories(path));
+            foreach (string subdirectory in dirList)
+            {
+                GetSubDirectories(subdirectory);
+                Console.Write("\n");
+            }
+        }
+
+        private static void GetSubDirectories(string dirStr)
+        {
+            Console.WriteLine(dirStr);
+            List<string> dirList = new List<string>(Directory.GetDirectories(dirStr));
+            foreach (string subdirectory in dirList)
+            {
+                Console.Write(" ");
+                GetSubDirectories(subdirectory);
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ HW json file/HW app/HW app/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace HW_app
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+        public Student(string name, string surname, byte age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+    }
+}
